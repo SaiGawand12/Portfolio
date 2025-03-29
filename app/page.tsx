7d665f299d516f9f972dcf733dbbd52c473c7da2
Sai Gawand <112680931+SaiGawand12@users.noreply.github.com>
2025-03-29 09:09:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
             <a href="SaiGawand_Cybersecurity_Resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-flex items-center px-6 py-3 bg-primary text-white rounded-lg hover:bg-secondary">
+              className="inline-flex items-center px-6 py-3 bg-primary/90 text-white rounded-lg hover:bg-secondary/90 transition duration-300 shadow-md">
               <Eye className="w-5 h-5 mr-2" /> View CV
             </a>
           </div>
