aa22632cce4b65f7402908d1a1c758c4d4307095
Sai Gawand <112680931+SaiGawand12@users.noreply.github.com>
2025-04-18 00:40:38 +0530

    Create Background3D.tsx

---
+++ components/Background3D.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import * as random from 'maath/random';
+
+function StarField() {
+  const ref = useRef<any>();
+  const positions = random.inSphere(new Float32Array(5000), { radius: 1.5 });
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={positions} stride={3} frustumCulled={false}>
+        <PointMaterial
+          transparent
+          color="#00ffff"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
+
+export default function Background3D() {
+  return (
+    <div className="absolute inset-0 -z-10">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <StarField />
+      </Canvas>
+    </div>
+  );
+}
